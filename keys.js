c249e93bc5ab1d2d5b577edc52dca087a68c0446
Aadhavan-Ramkumar <74062499+Aadhavan-Ramkumar@users.noreply.github.com>
2022-01-25 21:02:36 +0530

    Add files via upload

--- keys.js
+++ keys.js
@@ -1,63 +1,77 @@
-RedFocus = true;
-GreenFocus = false;
-BlueFocus = false;
-document.getElementById("RedTag").setAttribute("class", "Focused");
+ColorFocused = false;
+RedTag = [document.getElementById("RedTag"), document.getElementById("RedMinus"), document.getElementById("RedPlus")];
+GreenTag = [document.getElementById("GreenTag"), document.getElementById("GreenMinus"), document.getElementById("GreenPlus")];
+BlueTag = [document.getElementById("BlueTag"), document.getElementById("BlueMinus"), document.getElementById("BluePlus")];
+LastFocused = RedTag;
 
 window.addEventListener("keydown", MyKeyDown);
 
 function MyKeyDown(e) {
     KeyPressed = e.keyCode;
     switch (KeyPressed) {
+        case 32:
+            if (ColorFocused == false) {
+                ColorFocused = true;
+                LastFocused[0].setAttribute("class", "Focused");
+            } else {
+                ColorFocused = false;
+                LastFocused[0].removeAttribute("class", "Focused");
+            }
+            break;
         case 38:
-            if (GreenFocus == true) {
-                RedFocus = true;
-                GreenFocus = false;
-                BlueFocus = false;
-                document.getElementById("RedTag").setAttribute("class", "Focused");
-                document.getElementById("GreenTag").removeAttribute("class", "Focused");
-                document.getElementById("BlueTag").removeAttribute("class", "Focused");
-            } else if (BlueFocus == true) {
-                RedFocus = false;
-                GreenFocus = true;
-                BlueFocus = false;
-                document.getElementById("RedTag").removeAttribute("class", "Focused");
-                document.getElementById("GreenTag").setAttribute("class", "Focused");
-                document.getElementById("BlueTag").removeAttribute("class", "Focused");
+            if (ColorFocused == true) {
+                if (LastFocused == GreenTag) {
+                    LastFocused = RedTag;
+                    GreenTag[0].removeAttribute("class", "Focused");
+                    RedTag[0].setAttribute("class", "Focused");
+                } else if (LastFocused == BlueTag) {
+                    LastFocused = GreenTag;
+                    BlueTag[0].removeAttribute("class", "Focused");
+                    GreenTag[0].setAttribute("class", "Focused");
+                }
             }
             break;
         case 40:
-            if (RedFocus == true) {
-                RedFocus = false;
-                GreenFocus = true;
-                BlueFocus = false;
-                document.getElementById("RedTag").removeAttribute("class", "Focused");
-                document.getElementById("GreenTag").setAttribute("class", "Focused");
-                document.getElementById("BlueTag").removeAttribute("class", "Focused");
-            } else if (GreenFocus == true) {
-                RedFocus = false;
-                GreenFocus = false;
-                BlueFocus = true;
-                document.getElementById("RedTag").removeAttribute("class", "Focused");
-                document.getElementById("GreenTag").removeAttribute("class", "Focused");
-                document.getElementById("BlueTag").setAttribute("class", "Focused");
-            }
-            break;
-        case 39:
-            if (RedFocus == true && document.getElementById("RedPlus").disabled == false) {
-                Value("Increase", "Red");
-            } else if (GreenFocus == true && document.getElementById("GreenPlus").disabled == false) {
-                Value("Increase", "Green");
-            } else if (BlueFocus == true && document.getElementById("BluePlus").disabled == false) {
-                Value("Increase", "Blue");
+            if (ColorFocused == true) {
+                if (LastFocused == RedTag) {
+                    LastFocused = GreenTag;
+                    RedTag[0].removeAttribute("class", "Focused");
+                    GreenTag[0].setAttribute("class", "Focused");
+                } else if (LastFocused == GreenTag) {
+                    LastFocused = BlueTag;
+                    GreenTag[0].removeAttribute("class", "Focused");
+                    BlueTag[0].setAttribute("class", "Focused");
+                }
             }
             break;
         case 37:
-            if (RedFocus == true && document.getElementById("RedMinus").disabled == false) {
-                Value("Decrease", "Red");
-            } else if (GreenFocus == true && document.getElementById("GreenMinus").disabled == false) {
-                Value("Decrease", "Green");
-            } else if (BlueFocus == true && document.getElementById("BlueMinus").disabled == false) {
-                Value("Decrease", "Blue");
+            if (ColorFocused == true && LastFocused[1].disabled == false) {
+                switch (LastFocused) {
+                    case RedTag:
+                        Value("Decrease", "Red");
+                        break;
+                    case GreenTag:
+                        Value("Decrease", "Green");
+                        break;
+                    case BlueTag:
+                        Value("Decrease", "Blue");
+                        break;
+                }
+            }
+            break;
+        case 39:
+            if (ColorFocused == true && LastFocused[2].disabled == false) {
+                switch (LastFocused) {
+                    case RedTag:
+                        Value("Increase", "Red");
+                        break;
+                    case GreenTag:
+                        Value("Increase", "Green");
+                        break;
+                    case BlueTag:
+                        Value("Increase", "Blue");
+                        break;
+                }
             }
             break;
     }
